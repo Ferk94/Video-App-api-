326e469a4bef936ce742afe33d2cc2c763959e73
Fernando <Fernando.Kaganovicz@gmail.com>
2022-06-14 17:02:04 -0300

    add precharges categories

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const server = require("./src/app.js");
-const { conn, User } = require("./src/db.js");
-
+const { conn, User, Category } = require("./src/db.js");
+const categoryData = require('./src/data/categoryData');
 // const usersData = require("./src/data/usersData");
 
 conn.sync({ force: true }).then(() => {
   console.log("base de datos conectada");
 //   usersData.forEach((e) => User.create(e));
 //   console.log("usuarios precargados");
-
+  categoryData.forEach(e => Category.create(e));
+  console.log('Categorías precargadas');
   server.listen(3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
---
+++ src/data/categoryData.js
@@ -0,0 +1,36 @@
+
+
+const categories = [
+    {
+        name: 'Terror'
+    },
+    {
+        name: 'Suspenso'
+    },
+    {
+        name: 'Drama'
+    },
+    {
+        name: 'Comedia'
+    },
+    {
+        name: 'Acción'
+    },
+    {
+        name: 'Thriller'
+    },
+    {
+        name: 'Animados'
+    },
+    {
+        name: 'Documental'
+    },
+    {
+        name: 'Aventura'
+    },
+    {
+        name: 'Ciencia Ficción'
+    }
+]
+
+module.exports = categories;
