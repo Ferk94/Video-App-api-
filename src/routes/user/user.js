b3313b26c9d78f476cfb1c32dd80678fabfd403a
Fernando <Fernando.Kaganovicz@gmail.com>
2022-06-17 19:46:41 -0300

    create middlewares and controllers

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -1,70 +1,20 @@
 const express = require("express");
-
-const { User } = require("../../db");
-
 const jwt = require('jsonwebtoken');
+const { User } = require("../../db");
+const userControllers = require("../../controllers/userController");
+const verifySignUp = require('../../middlewares/verifySignUp');
+const verifyLogin = require('../../middlewares/verifyLogin');
+const authentication = require("../../middlewares/authentication");
 
 const router = express.Router();
 
 
-
-function verifyToken(req, res, next){
-  const bearerHeader = req.headers['authorization']
-
-  if(typeof bearerHeader !== 'undefined'){
-     const bearerToken = bearerHeader.split(" ")[1]
-     req.token = bearerToken;
-     next();
-  }else {
-       res.sendStatus(403)
-  }
-}
-
-
-
-
-
 // ruta para registrarse
-router.post("/signUp", (req, res, next) => {
-  const { email, password } = req.body;
-  console.log(email, password, 'llegan los datos?')
-  User.create({
-    email,
-    password
-  })
-    .then((newUser) => {
-      jwt.sign({newUser}, 'secretkey', {expiresIn: 86400}, (err, token) => {
-        res.json({newUser, token})
-      })
-    })
-    .catch((err) => next(err));
-});
+router.post('/signUp', [verifySignUp.checkEmailAndPassword], userControllers.signUp)
 
 
 //ruta para iniciar sesion
-router.post("/login", (req, res, next) => {
-  const { email, password } = req.body;
-  console.log(email, password, 'llegan los datos en el login?')
-  User.findOne({
-    where: {
-      email: email,
-      password: password,
-    },
-  })
-    .then((usuario) => {
-      const user = {
-        id: usuario.id,
-        name: usuario.name,
-        password: usuario.password,
-        role: usuario.role,
-      };
-      const token = jwt.sign({id: user.id}, "group8", {
-        expiresIn:86400
-    })
-      res.json({token, user});
-    })
-    .catch((err) => next(err));
-});
+router.post('/login', [verifyLogin.checkUser], userControllers.login)
 
 
 
