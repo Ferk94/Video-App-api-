ac91c3dfd92818b16faa65d6198b6b571972459d
Fernando <Fernando.Kaganovicz@gmail.com>
2022-06-20 19:40:33 -0300

    finish api

---
+++ src/data/contentData.js
@@ -0,0 +1,76 @@
+const data = [
+    {   
+        id: 1,
+        Title: 'Carmencita',
+        Released:'10 Sep 1994',
+        Genre: 'Documentary',
+        Director: 'Fernando Kaganovicz',
+        Poster: "https://m.media-amazon.com/images/M/MV5BZmUzOWFiNDAtNGRmZi00NTIxLWJiMTUtYzhkZGRlNzg1ZjFmXkEyXkFqcGdeQXVyNDE5MTU2MDE@._V1_SX300.jpg"
+    },
+    {   
+        id: 2,
+        Title: 'Carmencita',
+        Released:'10 Sep 1994',
+        Genre: 'Documentary',
+        Director: 'Fernando Kaganovicz',
+        Poster: "https://m.media-amazon.com/images/M/MV5BZmUzOWFiNDAtNGRmZi00NTIxLWJiMTUtYzhkZGRlNzg1ZjFmXkEyXkFqcGdeQXVyNDE5MTU2MDE@._V1_SX300.jpg"
+    },
+    {   
+        id: 3,
+        Title: 'Carmencita',
+        Released:'10 Sep 1994',
+        Genre: 'Documentary',
+        Director: 'Fernando Kaganovicz',
+        Poster: "https://m.media-amazon.com/images/M/MV5BZmUzOWFiNDAtNGRmZi00NTIxLWJiMTUtYzhkZGRlNzg1ZjFmXkEyXkFqcGdeQXVyNDE5MTU2MDE@._V1_SX300.jpg"
+    },
+    {   
+        id: 4,
+        Title: 'Carmencita',
+        Released:'10 Sep 1994',
+        Genre: 'Documentary',
+        Director: 'Fernando Kaganovicz',
+        Poster: "https://m.media-amazon.com/images/M/MV5BZmUzOWFiNDAtNGRmZi00NTIxLWJiMTUtYzhkZGRlNzg1ZjFmXkEyXkFqcGdeQXVyNDE5MTU2MDE@._V1_SX300.jpg"
+    },
+    {   
+        id: 5,
+        Title: 'Carmencita',
+        Released:'10 Sep 1994',
+        Genre: 'Documentary',
+        Director: 'Fernando Kaganovicz',
+        Poster: "https://m.media-amazon.com/images/M/MV5BZmUzOWFiNDAtNGRmZi00NTIxLWJiMTUtYzhkZGRlNzg1ZjFmXkEyXkFqcGdeQXVyNDE5MTU2MDE@._V1_SX300.jpg"
+    },
+    {   
+        id: 6,
+        Title: 'Carmencita',
+        Released:'10 Sep 1994',
+        Genre: 'Documentary',
+        Director: 'Fernando Kaganovicz',
+        Poster: "https://m.media-amazon.com/images/M/MV5BZmUzOWFiNDAtNGRmZi00NTIxLWJiMTUtYzhkZGRlNzg1ZjFmXkEyXkFqcGdeQXVyNDE5MTU2MDE@._V1_SX300.jpg"
+    },
+    {   
+        id: 7,
+        Title: 'Carmencita',
+        Released:'10 Sep 1994',
+        Genre: 'Documentary',
+        Director: 'Fernando Kaganovicz',
+        Poster: "https://m.media-amazon.com/images/M/MV5BZmUzOWFiNDAtNGRmZi00NTIxLWJiMTUtYzhkZGRlNzg1ZjFmXkEyXkFqcGdeQXVyNDE5MTU2MDE@._V1_SX300.jpg"
+    },
+    {   
+        id: 8,
+        Title: 'Carmencita',
+        Released:'10 Sep 1994',
+        Genre: 'Documentary',
+        Director: 'Fernando Kaganovicz',
+        Poster: "https://m.media-amazon.com/images/M/MV5BZmUzOWFiNDAtNGRmZi00NTIxLWJiMTUtYzhkZGRlNzg1ZjFmXkEyXkFqcGdeQXVyNDE5MTU2MDE@._V1_SX300.jpg"
+    },
+    {   
+        id: 9,
+        Title: 'Carmencita',
+        Released:'10 Sep 1994',
+        Genre: 'Documentary',
+        Director: 'Fernando Kaganovicz',
+        Poster: "https://m.media-amazon.com/images/M/MV5BZmUzOWFiNDAtNGRmZi00NTIxLWJiMTUtYzhkZGRlNzg1ZjFmXkEyXkFqcGdeQXVyNDE5MTU2MDE@._V1_SX300.jpg"
+    },
+]
+
+module.exports = data;
