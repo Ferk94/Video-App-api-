6fd766d8ab7d910141467a301b10c30c8a9e3ceb
Fernando <Fernando.Kaganovicz@gmail.com>
2022-06-19 23:31:44 -0300

    api data

--- index.js
+++ index.js
@@ -6,14 +6,62 @@ const categoryData = require('./src/data/categoryData');
 require("dotenv").config();
 const { API_KEY } = process.env;
 
-const API = `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}`
 
-conn.sync({ force: true }).then(() => {
+
+
+function firstIteration(numberString){
+  if(numberString[0] === '0' && numberString[1] === '0' && numberString[2] !== '9'){
+    return true;
+  }
+  return false;
+}
+
+// function isntNine(numberString){
+//   if(numberString[0] === '0' && numberString[1] === '0' && parseInt(numberString[2]) < 9){
+//     return true
+//   }
+//   return false;
+// }
+
+
+
+
+let data = [];
+let lastDigits = '001';
+let index = 1;
+
+console.log(data, ' se me guardan las pelis?')
+
+conn.sync({ force: true }).then( async () => {
   console.log("base de datos conectada");
-  // axios.get(API)
-  // .then(response => {
-    // var data = response.data;
-    // console.log(data, 'q hay aca?')
+  
+  console.log(lastDigits, 'subio el valor?')
+
+  for(let i = 0; i < index && index !== 5 ; i++){
+  
+    const API = `https://www.omdbapi.com/?i=tt0000${lastDigits}&apikey=${API_KEY}`
+    const isTrue = firstIteration(lastDigits)
+    if(isTrue === true){
+      const response =  await axios.get(API)
+      const apiData = response.data
+      data.push(apiData)
+      var number = parseInt(lastDigits[2])
+      console.log(number, 'el ultimo digito')
+      var suma = number + 1
+      let string = suma.toString()
+      console.log(string, 'q onda con el string a insertar?')
+      console.log(suma, ' sube el valor?')
+     lastDigits.replace(lastDigits[2], string )
+     console.log(lastDigits, ' q onda con el string de afuera?')
+     index ++
+    }
+    
+    
+    
+  }
+  console.log(data, 'q onda con la data')
+  console.log(index, 'va subiendo el index?')
+
     categoryData.forEach(e => Category.create(e));
     console.log('Categorías precargadas');
     server.listen(3001, () => {
